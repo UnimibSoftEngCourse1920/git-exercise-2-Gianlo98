6913905156a545ed58c3b06b344d22265fea9443
Gianlorenzo Occhipinti <g.occhipinti3@campus.unimib.it>
2019-10-22 09:52:46 +0200

    Changed Println message

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -2,6 +2,6 @@ package gitExercise2.main;
 
 public class HelloGit {
 	public static void main (String [] args) {
-		System.out.println("Hello git!!!");
+		System.out.println("And he have only one thing to say: PANCAKES!!! -MiniPEKKA");
 	}
 }
