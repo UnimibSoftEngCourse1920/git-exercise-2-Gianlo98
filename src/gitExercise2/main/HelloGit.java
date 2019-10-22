9ff10926d4d78a3acde02b845b83c84e55fe9110
Gianlorenzo Occhipinti <g.occhipinti3@campus.unimib.it>
2019-10-22 09:56:34 +0200

    Added new function from 2nd Developer

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,5 +3,11 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("And he have only one thing to say: PANCAKES!!! -MiniPEKKA");
+		randomPrint();
 	}
+	
+	public static void randomPrint(){
+		System.out.println("Real programmers use Emacs -Anonymous prof");
+	}
+	
 }
